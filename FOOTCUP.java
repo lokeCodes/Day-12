6cc102957df5d6c95d4bd2df0d4d91cb3b2bebcc
lokeCodes <125755158+lokeCodes@users.noreply.github.com>
2023-02-18 22:14:33 +0530

    Add files via upload

---
+++ FOOTCUP.java
@@ -0,0 +1,21 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner s = new Scanner(System.in);
+	    int t = s.nextInt();
+	    while(t-- > 0){
+	        int x = s.nextInt();
+	        int y = s.nextInt();
+	        String res = (x==0 && y==0 || x<y || y<x) ? "NO" : "YES" ;
+	        System.out.println(res);
+	    }
+	}
+}
---
+++ PROINC.java
@@ -0,0 +1,22 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s = new Scanner(System.in);
+		int t = s.nextInt();
+		while(t-- > 0){
+		    int x = s.nextInt();
+		    int y = s.nextInt();
+		    int x1 = (x/10)+x;
+		    int x2 = x-y;
+		    System.out.println(x1-x2);
+		}
+	}
+}
